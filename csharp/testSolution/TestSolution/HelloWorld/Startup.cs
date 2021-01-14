58fb1f4f8feddd9a0088006d042e1ed469b6ec1d
Nikolay Lyutakov <nikolaylutakov@gmail.com>
2021-01-15 01:47:58 +0200

    cshrp test project chaneged

--- csharp/testSolution/TestSolution/HelloWorld/Startup.cs
+++ csharp/testSolution/TestSolution/HelloWorld/Startup.cs
@@ -6,7 +6,12 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] numbers = new int[] { 2, 343, 5, 2, 42, 52, 32, 2, 3, 23, 423, 2 };
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
         }
     }
 }
