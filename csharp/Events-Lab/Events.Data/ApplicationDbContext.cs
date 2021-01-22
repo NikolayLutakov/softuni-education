37099d5b7eb8c416cba47910a8127e6a3e9155d5
Nikolay Lyutakov <nikolaylutakov@gmail.com>
2021-01-22 19:04:43 +0200

    add Events-Lab tutorial app on ASP.NET Framework

---
+++ csharp/Events-Lab/Events.Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Events.Data
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public IDbSet<Event> Events { get; set; }
+        public IDbSet<Comment> Comments { get; set; }
+
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
