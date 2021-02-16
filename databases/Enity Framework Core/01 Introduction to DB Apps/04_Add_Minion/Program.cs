1399421c9cea9c4a6865fce523b3cc172447d2d7
Nikolay Lyutakov <nikolaylutakov@gmail.com>
2021-02-17 01:03:53 +0200

    Added solutions for tas 4 and 5 from ADO.NET Exercise

--- databases/Enity Framework Core/01 Introduction to DB Apps/04_Add_Minion/Program.cs
+++ databases/Enity Framework Core/01 Introduction to DB Apps/04_Add_Minion/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Data.SqlClient;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace _04_Add_Minion
@@ -8,43 +9,142 @@ namespace _04_Add_Minion
     {
         static void Main(string[] args)
         {
-            string[] firstLineParams = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
-            string[] secondLineParams = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string minionName, minionTown, villainName;
+            int minionAge;
 
-            string minionName = firstLineParams[1];
-            int minionAge = int.Parse(firstLineParams[2]);
-            string minionTown = firstLineParams[3];
+            ProcessInput(out minionName, out minionAge, out minionTown, out villainName);
+            StartProcessing(minionName, minionTown, villainName, minionAge);
 
-            string villainName = secondLineParams[1];
+        }
+
+        private static void StartProcessing(string minionName, string minionTown, string villainName, int minionAge)
+        {
+            Dictionary<string, string> commands = new Dictionary<string, string>();
+            FuillCommands(commands);
+
+            int townId = 0;
+            int villainId = 0;
+            int minionId = 0;
 
             using (SqlConnection connection = new SqlConnection("Server=192.168.0.116,1433;Database=MinionsDB;User ID=nlyutakov;Password=A123456a;"))
             {
                 connection.Open();
 
-                string takeTownsQueryString = "SELECT Id, Name FROM Towns WHERE Name = @townName";
-                SqlCommand takeTownCmd = new SqlCommand(takeTownsQueryString, connection);
-                takeTownCmd.Parameters.AddWithValue("@townName", minionTown);
-                using (SqlDataReader townsResult = takeTownCmd.ExecuteReader())
-                {
-                    if (!townsResult.HasRows)
-                    {
-                        townsResult.Close();
-                        string insertTownQueryString = "INSERT INTO Towns (Name) VALUES (@townName)";
-                        SqlCommand insertTownCmd = new SqlCommand(insertTownQueryString, connection);
-                        insertTownCmd.Parameters.AddWithValue("@townName", minionTown);
+                townId = ProcessTown(minionTown, commands, connection);
+                villainId = ProcessVillain(villainName, commands, connection);
 
-                        insertTownCmd.ExecuteNonQuery();
-
-                        Console.WriteLine($"Town {minionTown} was added to the database.");
-                    }
-                    else
-                    {
-
-                    }
-                }
-
-
+                minionId = ProcessMinion(minionName, minionAge, villainName, commands, townId, villainId, connection);
+                AddMinion(minionName, villainName, commands, villainId, minionId, connection);
             }
+        }
+
+        private static void ProcessInput(out string minionName, out int minionAge, out string minionTown, out string villainName)
+        {
+            string[] firstLineParams = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string[] secondLineParams = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            minionName = firstLineParams[1];
+            minionAge = int.Parse(firstLineParams[2]);
+            minionTown = firstLineParams[3];
+            villainName = secondLineParams[1];
+        }
+
+        private static void AddMinion(string minionName, string villainName, Dictionary<string, string> commands, int villainId, int minionId, SqlConnection connection)
+        {
+            SqlCommand insertMinionsVillains = new SqlCommand(commands["insertMinionsVillains"], connection);
+            insertMinionsVillains.Parameters.AddWithValue("@villainId", villainId);
+            insertMinionsVillains.Parameters.AddWithValue("@minionId", minionId);
+
+            insertMinionsVillains.ExecuteNonQuery();
+
+            Console.WriteLine($"Successfully added {minionName} to be minion of {villainName}.");
+        }
+
+        private static int ProcessMinion(string minionName, int minionAge, string villainName, Dictionary<string, string> commands, int townId, int villainId, SqlConnection connection)
+        {
+            int minionId;
+            SqlCommand insertMinion = new SqlCommand(commands["insertMinion"], connection);
+            insertMinion.Parameters.AddWithValue("@name", minionName);
+            insertMinion.Parameters.AddWithValue("@age", minionAge);
+            insertMinion.Parameters.AddWithValue("@townId", townId);
+
+            insertMinion.ExecuteNonQuery();
+
+            SqlCommand takeInsertedMinionId = new SqlCommand(commands["selectMinionId"], connection);
+            takeInsertedMinionId.Parameters.AddWithValue("@Name", minionName);
+
+            minionId = (int)takeInsertedMinionId.ExecuteScalar();
+
+
+            return minionId;
+        }
+
+        private static int ProcessVillain(string villainName, Dictionary<string, string> commands, SqlConnection connection)
+        {
+            int villainId;
+            SqlCommand takeVillainId = new SqlCommand(commands["selectVillains"], connection);
+            takeVillainId.Parameters.AddWithValue("@Name", villainName);
+
+            int? takenVillainId = (int?)takeVillainId.ExecuteScalar();
+
+            if (takenVillainId == null)
+            {
+                SqlCommand insertVillain = new SqlCommand(commands["insertVillain"], connection);
+                insertVillain.Parameters.AddWithValue("@villainName", villainName);
+                insertVillain.ExecuteNonQuery();
+
+                SqlCommand takeInsertedVillainId = new SqlCommand(commands["selectVillains"], connection);
+                takeInsertedVillainId.Parameters.AddWithValue("@Name", villainName);
+                villainId = (int)takeInsertedVillainId.ExecuteScalar();
+                Console.WriteLine($"Villain {villainName} was added to the database.");
+            }
+            else
+            {
+                villainId = (int)takenVillainId;
+            }
+
+            return villainId;
+        }
+
+        private static int ProcessTown(string minionTown, Dictionary<string, string> commands, SqlConnection connection)
+        {
+            int townId;
+            SqlCommand takeTownId = new SqlCommand(commands["selectTownId"], connection);
+            takeTownId.Parameters.AddWithValue("@townName", minionTown);
+
+            int? takenTownId = (int?)takeTownId.ExecuteScalar();
+
+            if (takenTownId == null)
+            {
+                SqlCommand insertTown = new SqlCommand(commands["insertTown"], connection);
+                insertTown.Parameters.AddWithValue("@townName", minionTown);
+                insertTown.ExecuteNonQuery();
+
+                SqlCommand takeInsertedTownId = new SqlCommand(commands["selectTownId"], connection);
+                takeInsertedTownId.Parameters.AddWithValue("@townName", minionTown);
+
+                townId = (int)takeInsertedTownId.ExecuteScalar();
+
+                Console.WriteLine($"Town {minionTown} was added to the database.");
+            }
+            else
+            {
+                townId = (int)takenTownId;
+            }
+
+            return townId;
+        }
+
+        private static Dictionary<string, string> FuillCommands(Dictionary<string, string> commands)
+        {
+            commands["selectVillains"] = "SELECT Id FROM Villains WHERE Name = @Name";
+            commands["selectMinionId"] = "SELECT Id FROM Minions WHERE Name = @Name";
+            commands["insertMinionsVillains"] = "INSERT INTO MinionsVillains(MinionId, VillainId) VALUES(@minionId, @villainId)";
+            commands["insertVillain"] = "INSERT INTO Villains(Name, EvilnessFactorId)  VALUES(@villainName, 4)";
+            commands["insertMinion"] = "INSERT INTO Minions(Name, Age, TownId) VALUES(@name, @age, @townId)";
+            commands["insertTown"] = "INSERT INTO Towns(Name) VALUES(@townName)";
+            commands["selectTownId"] = "SELECT Id FROM Towns WHERE Name = @townName";
+            return commands;
         }
     }
 }
---
+++ databases/Enity Framework Core/01 Introduction to DB Apps/05_Change_Town_Names_Casing/Program.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+
+namespace _05_Change_Town_Names_Casing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string updateQueryString = @"UPDATE Towns
+                                         SET Name = UPPER(Name)
+                                         WHERE CountryCode = (SELECT c.Id FROM Countries AS c WHERE c.Name =                    @countryName)";
+
+            string selectUpdatedQueryString = @"SELECT t.Name
+                                        FROM Towns as t
+                                        JOIN Countries AS c ON c.Id = t.CountryCode
+                                        WHERE c.Name = @countryName";
+
+            using (SqlConnection connection = new SqlConnection("Server=192.168.0.116,1433;Database=MinionsDB;User ID=nlyutakov;Password=A123456a;"))
+            {
+                connection.Open();
+                SqlCommand updateTownsCmd = new SqlCommand(updateQueryString, connection);
+                updateTownsCmd.Parameters.AddWithValue("@countryName", input);
+
+                
+
+                int updatedTowns = updateTownsCmd.ExecuteNonQuery();
+
+                if (updatedTowns == 0)
+                {
+                    Console.WriteLine("No town names were affected.");
+                }
+                else
+                {
+                    SqlCommand selectUpdatedTownsCmd = new SqlCommand(selectUpdatedQueryString, connection);
+                    selectUpdatedTownsCmd.Parameters.AddWithValue("@countryName", input);
+
+                    List<string> townNames = new List<string>();
+
+                    using (var result = selectUpdatedTownsCmd.ExecuteReader())
+                    {
+                        while (result.Read())
+                        {
+                            townNames.Add((string)result["Name"]);
+                        }
+                    }
+                    Console.WriteLine($"{updatedTowns} town names were affected.");
+                    Console.WriteLine($"[{string.Join(", ", townNames)}]");
+
+                }
+            }
+        }
+    }
+}
